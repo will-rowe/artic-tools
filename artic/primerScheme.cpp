9b59a11b51bda3df238fec97db8f90eb2e426367
Will Rowe <w.p.m.rowe@gmail.com>
2020-08-20 14:24:39 +0100

    fixing gcc fail

--- artic/primerScheme.cpp
+++ artic/primerScheme.cpp
@@ -168,7 +168,10 @@ unsigned int artic::PrimerScheme::GetNumAmplicons(void) { return _numAmplicons; 
 unsigned int artic::PrimerScheme::GetMeanAmpliconSpan(void) { return _meanAmpliconSpan; }
 
 // GetPrimerPools returns the primer pools found in the primer scheme.
-std::vector<std::string> artic::PrimerScheme::GetPrimerPools(void) { return std::vector(_primerPools.begin() + 1, _primerPools.end()); }
+std::vector<std::string> artic::PrimerScheme::GetPrimerPools(void)
+{
+    return std::vector<std::string>(_primerPools.begin() + 1, _primerPools.end());
+}
 
 // GetPrimerPool returns the primer pool for the provided pool ID.
 const std::string& artic::PrimerScheme::GetPrimerPool(size_t poolID) const
